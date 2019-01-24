ba6504b870cc6ab876556e839d0640456caf71aa
Luke Parker <distributednormally@gmail.com>
2019-01-24 11:34:05 -0500

    commit for app change

--- Day 6/app.js
+++ Day 6/app.js
@@ -9,16 +9,18 @@ const shopRoutes = require('./routes/shop');
 
 const bodyParser = require('body-parser');
 
+const path = require('path');
+
 const app = express();
 
 app.use(bodyParser.urlencoded());
 
-//All admin routes will now be precedd by /admin/
+// All admin routes will now be precedd by /admin/
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, '../', 'views', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
