fa6574503404c6e56adc6b7da21aaabd826f44b9
Luke Parker <lparker.tpx@gmail.com>
2019-02-01 15:26:59 -0500

    EJS templating engine intro --- native to Node. 404 is working.

---
+++ Day 9/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const adminData = require('./routes/admin');
+
+const rootDir = require('./util/path');
+
+console.log('LOG | ', rootDir.toString());
+
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
+
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// All admin routes will now be preceded by /admin/
+app.use('/admin', adminData.routes);
+app.use('/add-product', (req, res, next) => {
+  res.redirect('/admin/add-product');
+});
+
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.render('404', {pageTitle: "Hbs not found :("});
+});
+
+app.listen(3000);
---
+++ Day 9/routes/shop.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    console.log('shop.js', adminData.products);
+    const products = adminData.products;
+
+    //Instead of sending shop.html, use the templating engine PUG.
+    res.render('shop', {
+        prods: products,
+        pageTitle: 'Shopping',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
+});
+
+module.exports = router;
